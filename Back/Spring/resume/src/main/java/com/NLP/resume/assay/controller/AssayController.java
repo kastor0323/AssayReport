e3b914bdf2f96d3d206fd0605b90fca3109183e9
JungHyun Beak <kastor0323@gmail.com>
2025-06-02 22:13:23 +0900

    Back_SB ) FIX ) SaveAssay 로직 구현

--- Back/Spring/resume/src/main/java/com/NLP/resume/assay/controller/AssayController.java
+++ Back/Spring/resume/src/main/java/com/NLP/resume/assay/controller/AssayController.java
@@ -1,38 +1,33 @@
 package com.NLP.resume.assay.controller;
 
-import com.NLP.resume.member.domain.User;
-import com.NLP.resume.member.dto.SignUpRequest;
-import com.NLP.resume.member.dto.SignUpResponse;
+import com.NLP.resume.assay.dto.SaveAssayRequest;
+import com.NLP.resume.assay.dto.SaveAssayResponse;
+import com.NLP.resume.assay.service.AssayService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RequiredArgsConstructor
+@RestController
 public class AssayController {
 
-  @PostMapping("/resume/signup")
-  public ResponseEntity<SignUpResponse> signUp(@Valid @RequestBody SignUpRequest request) {
+  private final AssayService assayService;
+
+  @PostMapping("/resume/assay/save")
+  public ResponseEntity<SaveAssayResponse> signUp(@Valid @RequestBody SaveAssayRequest request, Authentication authentication) {
     try {
-      User newUser = request.toEntity();
-      User savedUser = userService.signUp(newUser);
+      String user_id = authentication.getName();
+      request.setUser_id(user_id);
 
-      return ResponseEntity.ok(
-          new SignUpResponse("회원가입 성공", savedUser.getUser_id(), true)
-      );
+      SaveAssayResponse response = assayService.saveAssay(request); // 파라미터 1개만 전달
+      return ResponseEntity.ok(response);
     } catch (Exception e) {
-      // 이미 존재하는 아이디 예외 체크 (예외 메시지로 판단)
-      if (e.getMessage() != null && e.getMessage().contains("이미 존재하는 아이디")) {
-        return ResponseEntity
-            .status(HttpStatus.CONFLICT) // 409 Conflict
-            .body(new SignUpResponse("이미 존재하는 아이디입니다", request.getUser_id(), false));
-      }
-
-      // 기타 서버 에러
-      return ResponseEntity
-          .status(HttpStatus.INTERNAL_SERVER_ERROR)
-          .body(new SignUpResponse("회원가입 중 오류가 발생했습니다: " + e.getMessage(),
-              request.getUser_id(), false));
+      return ResponseEntity.badRequest().build();
     }
   }
 }
